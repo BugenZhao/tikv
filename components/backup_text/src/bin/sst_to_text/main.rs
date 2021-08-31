aa9a6c39f3497ce816262db37dbe140eb9266b58
Bugen Zhao <i@bugenzhao.com>
2021-08-31 07:42:18 +0000

    cleanup & fix

--- components/backup_text/src/bin/sst_to_text/main.rs
+++ components/backup_text/src/bin/sst_to_text/main.rs
@@ -8,15 +8,16 @@ use std::{
 };
 
 use anyhow::{anyhow, Result};
-use backup_text::{decode_key, rwer::TextWriter};
+use backup_text::rwer::TextWriter;
 use collections::HashMap;
 use engine_rocks::RocksSstReader;
 use engine_traits::{name_to_cf, Iterator, SeekKey, SstReader, CF_DEFAULT, CF_WRITE};
 use external_storage_export::{create_storage, make_local_backend};
-use futures::{future::join_all, AsyncReadExt};
-use keys::data_key;
+use futures::{future::join_all, AsyncReadExt, FutureExt};
 use kvproto::brpb::{BackupMeta, File};
 use protobuf::Message;
+use slog::Drain;
+use slog_global::{info, warn};
 use structopt::StructOpt;
 use tidb_query_datatype::codec::table::decode_table_id;
 use tipb::TableInfo;
@@ -42,32 +43,26 @@ fn rewrite(
     file: &File,
     table_info: TableInfo,
 ) -> Result<()> {
-    let start_key = file.get_start_key();
-    let end_key = file.get_end_key();
-
     let path = {
         let mut path = PathBuf::from(dir.as_ref());
         path.push(file.get_name());
         path
     };
-    let reader = RocksSstReader::open(path.to_str().unwrap())?;
+    let path_str = path.to_str().unwrap();
+    let reader = RocksSstReader::open(path_str)?;
     reader.verify_checksum()?;
 
     let mut iter = reader.iter();
-    iter.seek(SeekKey::Key(&data_key(start_key)))?;
-    // assert_eq!(start_key, decode_key(iter.key()).0); // todo: why not eq?
+    iter.seek(SeekKey::Start)?; // ignore start_key in file
 
     let cf = name_to_cf(file.get_cf()).ok_or_else(|| anyhow!("bad cf name"))?;
-    let name = format!("{}_rewrite", path.to_str().unwrap());
+    let name = format!("{}_rewrite", path_str);
     let mut writer = TextWriter::new(table_info, cf, &name)?;
     let temp_name = writer.name().to_owned();
 
     let mut count = 0;
     while iter.valid()? {
         let key = iter.key();
-        if decode_key(key).0.as_slice() >= end_key { // todo: really needed?
-            break;
-        }
         let value = match cf {
             CF_WRITE => {
                 let write = WriteRef::parse(iter.value())?;
@@ -83,41 +78,41 @@ fn rewrite(
     }
 
     if count != file.get_total_kvs() {
-        println!(
-            "wrong count on {}: count={}, total_kvs={}",
-            path.to_str().unwrap(),
-            count,
-            file.get_total_kvs()
+        warn!(
+            "kv pairs count mismatched";
+            "file" => path_str,
+            "count" => count,
+            "expected" => file.get_total_kvs()
         );
     }
 
-    let reader = writer.finish_read().unwrap();
+    let reader = writer.finish_read()?;
     drop(reader);
 
-    let file_name = path.to_str().unwrap().replace(".sst", ".rewrite.txt");
+    let file_name = path_str.replace(".sst", ".rewrite.txt");
     fs::rename(&temp_name, &file_name)?;
 
     Ok(())
 }
 
-async fn worker() {
+async fn worker() -> Result<()> {
     let Opt { path } = Opt::from_args();
     let backend = make_local_backend(&path);
-    let storage = create_storage(&backend).unwrap();
+    let storage = create_storage(&backend)?;
 
     let meta = {
         let mut meta_reader = storage.read(META_FILE);
         let mut buf = vec![];
-        meta_reader.read_to_end(&mut buf).await.unwrap();
+        meta_reader.read_to_end(&mut buf).await?;
         let mut meta = BackupMeta::new();
-        meta.merge_from_bytes(&buf).unwrap();
+        meta.merge_from_bytes(&buf)?;
         meta
     };
 
     let file_map = {
         let mut map: HashMap<i64, Vec<File>> = Default::default();
         for file in read_data_files(&storage, &meta).await {
-            let table_id = decode_table_id(file.get_start_key()).unwrap();
+            let table_id = decode_table_id(file.get_start_key())?;
             map.entry(table_id).or_default().push(file);
         }
         map
@@ -141,14 +136,26 @@ async fn worker() {
                 rewrite(table_id, dir, &file, table_info)
                     .map_err(|e| anyhow!("failed to rewrite file {}: {}", file.get_name(), e))
                     .unwrap();
+                info!("rewrite done"; "file" => file.get_name());
             });
             handles.push(handle);
         }
     }
+
     join_all(handles).await;
+    info!("all done");
+
+    Ok(())
 }
 
 fn main() {
     let runtime = Runtime::new().unwrap();
-    runtime.block_on(worker())
+    let logger = {
+        let decorator = slog_term::PlainSyncDecorator::new(std::io::stdout());
+        let drain = slog_term::CompactFormat::new(decorator).build();
+        let drain = std::sync::Mutex::new(drain).fuse();
+        slog::Logger::root(drain, slog::o!())
+    };
+    slog_global::set_global(logger);
+    runtime.block_on(worker().map(Result::unwrap))
 }
