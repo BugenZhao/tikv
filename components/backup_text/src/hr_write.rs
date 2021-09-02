bfea03c37634b7f353430454efaae630f07573b6
Bugen Zhao <i@bugenzhao.com>
2021-09-02 06:58:46 +0000

    use serde remote for `WriteType`

--- components/backup_text/src/hr_write.rs
+++ components/backup_text/src/hr_write.rs
@@ -36,19 +36,20 @@ pub enum HrShortValue {
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct HrWrite {
-    #[serde(rename = "t")]
-    pub write_type: HrWriteType,
+    #[serde(rename = "t", with = "HrWriteType")]
+    pub write_type: WriteType,
     #[serde(rename = "s")]
     pub start_ts: u64,
     #[serde(rename = "o")]
     pub has_overlapped_rollback: bool,
-    #[serde(rename = "f")]
+    #[serde(rename = "f", skip_serializing_if = "Option::is_none", default)]
     pub gc_fence: Option<u64>,
     #[serde(rename = "v", skip_serializing_if = "Option::is_none", default)]
     pub value: Option<HrShortValue>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(remote = "WriteType")]
 pub enum HrWriteType {
     #[serde(rename = "p")]
     Put,
@@ -58,28 +59,6 @@ pub enum HrWriteType {
     Lock,
     #[serde(rename = "r")]
     Rollback,
-}
-
-impl From<WriteType> for HrWriteType {
-    fn from(wt: WriteType) -> HrWriteType {
-        match wt {
-            WriteType::Put => HrWriteType::Put,
-            WriteType::Delete => HrWriteType::Delete,
-            WriteType::Lock => HrWriteType::Lock,
-            WriteType::Rollback => HrWriteType::Rollback,
-        }
-    }
-}
-
-impl Into<WriteType> for HrWriteType {
-    fn into(self) -> WriteType {
-        match self {
-            HrWriteType::Put => WriteType::Put,
-            HrWriteType::Delete => WriteType::Delete,
-            HrWriteType::Lock => WriteType::Lock,
-            HrWriteType::Rollback => WriteType::Rollback,
-        }
-    }
 }
 
 impl HrWrite {
@@ -122,7 +101,7 @@ impl HrWrite {
             }
         });
         HrWrite {
-            write_type: HrWriteType::from(wr.write_type),
+            write_type: wr.write_type,
             start_ts: wr.start_ts.into_inner(),
             has_overlapped_rollback: wr.has_overlapped_rollback,
             gc_fence: wr.gc_fence.map(|t| t.into_inner()),
@@ -138,7 +117,7 @@ impl HrWrite {
             HrShortValue::Record(val) => val.into_bytes(ctx).unwrap(),
         });
         let write = Write {
-            write_type: self.write_type.into(),
+            write_type: self.write_type,
             start_ts: TimeStamp::from(self.start_ts),
             short_value,
             has_overlapped_rollback: self.has_overlapped_rollback,
